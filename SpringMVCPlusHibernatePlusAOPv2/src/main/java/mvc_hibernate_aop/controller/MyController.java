e5d2626a2fdc5c3a755ceffe467e302bdf32f24a
SoulsArchitecht <serje_fox@mail.ru>
2021-11-26 21:31:16 +0800

    SpringMVCPlusHibernatePlusAOPv3

--- SpringMVCPlusHibernatePlusAOPv2/src/main/java/mvc_hibernate_aop/controller/MyController.java
+++ SpringMVCPlusHibernatePlusAOPv2/src/main/java/mvc_hibernate_aop/controller/MyController.java
@@ -2,10 +2,13 @@ package mvc_hibernate_aop.controller;
 
 import mvc_hibernate_aop.dao.EmployeeDAO;
 import mvc_hibernate_aop.entity.Employee;
+import mvc_hibernate_aop.sevice.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,14 +16,47 @@ import java.util.List;
 public class MyController {
 
     @Autowired
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     @RequestMapping("/")
     public String ShowAllEmployee (Model model) {
 
-        List<Employee> allEmployees = employeeDAO.getAllEmployees();
+        List<Employee> allEmployees = employeeService.getAllEmployees();
         model.addAttribute("allEmps", allEmployees);
 
         return "all-employees"; // возвращает название view
+    }
+
+    @RequestMapping("/addNewEmployee")
+    public String addNewEmployee(Model model) {
+
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+
+    @RequestMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/updateInfo")
+    public String updateEmployee(@RequestParam("empId") int id, Model model) {
+
+        Employee employee = employeeService.getEmployee(id);
+        model.addAttribute("employee", employee);
+
+        return "employee-info";
+    }
+
+    @RequestMapping("/deleteEmployee")
+    public String deleteEmployee(@RequestParam("empId") int id) {
+
+        Employee employee = employeeService.getEmployee(id);
+
+        employeeService.deleteEmployee(id);
+
+        return "redirect:/";
     }
 }
--- SpringMVCPlusHibernatePlusAOPv2/src/main/java/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
+++ SpringMVCPlusHibernatePlusAOPv2/src/main/java/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -16,7 +16,8 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     private SessionFactory sessionFactory;
 
     @Override
-    @Transactional //перекладывает на спринг все дела по открытию и закрытию сессий
+    //@Transactional //перекладывает на спринг все дела по открытию и закрытию сессий
+    // перенесена в сервис
     public List<Employee> getAllEmployees() {
         Session session = sessionFactory.getCurrentSession();
         /*List<Employee> allEmployees = session.createQuery("from Employee", Employee.class)
@@ -25,4 +26,32 @@ public class EmployeeDAOImpl implements EmployeeDAO {
         List<Employee> allEmployees = query.getResultList();
         return allEmployees;
     }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        Employee employee = session.get(Employee.class, id);
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+/*        Employee employee = session.get(Employee.class, id);
+        session.delete(employee);*/
+
+        Query<Employee> query = session.createQuery("delete from Employee where id=:employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
+    }
+
+
 }
