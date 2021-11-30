ecb1bfef4a642c5353ed02dd807d95064f5318ee
SoulsArchitecht <serje_fox@mail.ru>
2021-11-30 22:14:21 +0800

    Spring_REST

---
+++ Spring_rest/src/main/java/spring_rest/Main.java
@@ -0,0 +1,4 @@
+package spring_rest;
+
+public class Main {
+}
---
+++ Spring_rest/src/main/java/spring_rest/controller/MyRESTController.java
@@ -0,0 +1,71 @@
+package spring_rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import spring_rest.entity.Employee;
+import spring_rest.exception_handling.EmployeeIncorrectData;
+import spring_rest.exception_handling.NoSuchEmployeeException;
+import spring_rest.sevice.EmployeeService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return  allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee (@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        if(employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = "
+                    + id + " in DataBase");
+        }
+        return employee; // в теле запроса будет не сам объект а его представление в JSON
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee (@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee (@RequestBody Employee employee) {
+
+        if(employee == null) {
+            throw new NoSuchEmployeeException("There is no such employee in DataBase");
+        }
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if(employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = "
+                    + id + " in DataBase");
+        }
+
+        employeeService.deleteEmployee(id);
+        return "Employee with ID = " + id + " was deleted";
+    }
+
+
+
+}
